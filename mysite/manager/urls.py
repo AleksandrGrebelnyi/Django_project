afda368a2b4d29bd96d34d1ed5f55094b2069e6e
Aleksandr <aleksandr955@gmail.com>
2022-07-30 09:53:35 +0300

    add actions

--- mysite/manager/urls.py
+++ mysite/manager/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import manager_general, manager, manager_menu
+from .views import manager_general, manager, manager_menu, manager_events, manager_gallary, book_a_table
 
 urlpatterns = [
     path('', manager_general),
     path('menu/', manager_menu),
+    path('events/', manager_events),
+    path('gallary/', manager_gallary),
+    path('book-a-table/',  book_a_table),
 ]
--- mysite/manager/views.py
+++ mysite/manager/views.py
@@ -4,7 +4,16 @@ def manager(requesr):
     return HttpResponse(f'Manager')
 
 def manager_general(request):
-    return HttpResponse('Base page of Manager')
+    return HttpResponse('Base page of MANAGER')
 
 def manager_menu(request):
-    return HttpResponse('Manager access to menu')
+    return HttpResponse('Manager access to MENU')
+
+def manager_events(request):
+    return HttpResponse('Manager access to EVENTS')
+
+def manager_gallary(request):
+    return HttpResponse('Manager access to GALLARY')
+
+def book_a_table(request):
+    return HttpResponse('Manager access to BOOK TABLE')
