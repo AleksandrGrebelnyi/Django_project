21ee3f0d0fd222d9a80d1addf9a373b75256863b
Aleksandr <aleksandr955@gmail.com>
2022-07-31 07:48:47 +0300

    fix

--- mysite/manager/urls.py
+++ mysite/manager/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', manager_general),
     path('menu/', manager_menu),
     path('menu/add-dish', add_dish, name='add-dish'),
-    path('menu/delete-dish', delete_dish, name='delete-dish'),
+    path('menu/delete-dish/<str:dish_name>', delete_dish, name='delete-dish'),
     path('events/', manager_events),
     path('events/add-event/', add_event, name='add-event'),
     path('events/add-event/addrecord', addrecord, name='addrecord'),
