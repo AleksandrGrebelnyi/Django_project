e5ec1bbf3833ca09f4256fef366c9b3ece2ca84b
Aleksandr <aleksandr955@gmail.com>
2022-08-03 15:23:42 +0300

    created models of data

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Models):
+    name = models.CharField(unique=True, max_length=50, db_index=True)
+    position = models.SmallIntegerField(unique=True)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ('position')
+
+
+class Dish(models.Model):
+    name = models.CharField(unique=True, max_length=50, db_index=True)
+    description = models.TextField('Description')
+    position = models.SmallIntegerField(unique=True)
+    price = models.FloatField('Price')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ('position')
+
+
+class Events(models.Model):
+    name = models.CharField(unique=True, max_length=50, db_index=True)
+    description = models.TextField('Description')
+    price = models.FloatField('Price')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ('name')
+
+
+class Gallery(models.Model):
+    name = models.CharField(unique=True, max_length=50, db_index=True)
+    photo = models.ImageField(width_field=50, height_field=50)
+
+    def __str__(self):
+        return f'{self.name}'
